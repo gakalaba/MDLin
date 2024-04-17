9b168f901e54f7c6ad8311f71336d050a22ed94f
gakalaba <akalaba@princeton.edu>
2024-04-17 17:05:41 -0600

    benching f16 with 9 shards batching

--- run_these_5ms.sh
+++ run_these_5ms.sh
@@ -14,7 +14,7 @@ ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/100us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_100us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -22,7 +22,7 @@ sed -i 's/"batchsize": 100,/"batchsize": 150,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/150us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_150us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -31,7 +31,7 @@ sed -i 's/"batchsize": 150,/"batchsize": 200,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_200us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_200us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -39,7 +39,7 @@ sed -i 's/"batchsize": 200,/"batchsize": 250,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_250us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_250us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -47,7 +47,7 @@ sed -i 's/"batchsize": 250,/"batchsize": 300,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_300us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_300us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -56,7 +56,7 @@ sed -i 's/"batchsize": 300,/"batchsize": 350,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_350us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_350us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -65,7 +65,7 @@ sed -i 's/"batchsize": 350,/"batchsize": 400,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_400us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_400us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -74,7 +74,7 @@ sed -i 's/"batchsize": 400,/"batchsize": 450,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_450us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_450us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -83,7 +83,7 @@ sed -i 's/"batchsize": 450,/"batchsize": 500,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_500us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_500us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -92,7 +92,7 @@ sed -i 's/"batchsize": 500,/"batchsize": 550,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_550us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_550us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -101,7 +101,7 @@ sed -i 's/"batchsize": 550,/"batchsize": 600,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_600us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_600us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -110,7 +110,7 @@ sed -i 's/"batchsize": 600,/"batchsize": 700,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_700us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_700us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -118,7 +118,7 @@ sed -i 's/"batchsize": 700,/"batchsize": 800,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_800us
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_800us
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
@@ -126,7 +126,7 @@ sed -i 's/"batchsize": 800,/"batchsize": 1000,/g' experiments/osdi24/bf4.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/bf4.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/f2_1ms
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/batching/f16_1ms
 sudo rm -rf /mnt/ex*/e*/2*
 ssh client-3-0 'sudo rm -rf /mnt/ex*/e*/2*'
 ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
