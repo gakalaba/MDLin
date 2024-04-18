40087ac24abc9c7651ed9e1c6d459638aef02e43
gakalaba <akalaba@princeton.edu>
2024-04-17 20:03:03 -0600

    ran multi-core with 9 shards - good latency, tput is weird

--- run_these_5ms.sh
+++ run_these_5ms.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 sudo rm -rf /mnt/ex*/e*/2*
 
-#python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1smc-mp.json
-#sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-#sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/multicore/f1
-#sudo rm -rf /mnt/ex*/e*/2*
-#sed -i 's/"client_fanout": 1,/"client_fanout": 2,/g' experiments/osdi24/test-1smc-mp.json
+python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1s-mp.json
+sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/1s/f1
+sudo rm -rf /mnt/ex*/e*/2*
+sed -i 's/"client_fanout": 1,/"client_fanout": 2,/g' experiments/osdi24/test-1s-mp.json
+
 #ssh client-1-0 'sudo rm -rf /mnt/ex*/e*/2*'
 #ssh client-2-0 'sudo rm -rf /mnt/ex*/e*/2*'
 
-#python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1smc-mp.json
-#sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-#sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/multicore/f2
-#sudo rm -rf /mnt/ex*/e*/2*
-#sed -i 's/"client_fanout": 2,/"client_fanout": 4,/g' experiments/osdi24/test-1smc-mp.json
+python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1s-mp.json
+sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/1s/f2
+sudo rm -rf /mnt/ex*/e*/2*
+sed -i 's/"client_fanout": 2,/"client_fanout": 4,/g' experiments/osdi24/test-1s-mp.json
 
 python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1s-mp.json
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
@@ -50,17 +51,17 @@ python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1s-mp.js
 sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
 sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/1s/f128
 sudo rm -rf /mnt/ex*/e*/2*
-#sed -i 's/"client_fanout": 128,/"client_fanout": 24,/g' experiments/osdi24/test-1smc-mp.json
+sed -i 's/"client_fanout": 128,/"client_fanout": 24,/g' experiments/osdi24/test-1s-mp.json
 
-#python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1smc-mp.json
-#sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-#sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/multicore/f24
-#sudo rm -rf /mnt/ex*/e*/2*
-#sed -i 's/"client_fanout": 24,/"client_fanout": 48,/g' experiments/osdi24/test-1smc-mp.json
+python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1s-mp.json
+sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/1s/f24
+sudo rm -rf /mnt/ex*/e*/2*
+sed -i 's/"client_fanout": 24,/"client_fanout": 48,/g' experiments/osdi24/test-1s-mp.json
 
-#python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1smc-mp.json
-#sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
-#sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/multicore/f48
-#sudo rm -rf /mnt/ex*/e*/2*
-#sed -i 's/"client_fanout": 48,/"client_fanout": 1,/g' experiments/osdi24/test-1smc-mp.json
+python3 ./scripts/run_multiple_experiments.py ./experiments/osdi24/test-1s-mp.json
+sudo rm -rf experiments/osdi24/results/tput-3/20*/2*/2*/out/cli*
+sudo mv experiments/osdi24/results/tput-3/20* /proj/praxis-PG0/exp/mdl/SOSP24/1s/f48
+sudo rm -rf /mnt/ex*/e*/2*
+sed -i 's/"client_fanout": 48,/"client_fanout": 1,/g' experiments/osdi24/test-1s-mp.json
 
