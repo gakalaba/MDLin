59aa1f384ec6b1283d7dda00e8fba7b2b0cd11e8
Austin <0austinli4@gmail.com>
2024-12-10 09:01:40 -0500

    python client add

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .client import AppRequest
+
+__all__ = ['AppRequest']
---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup, find_packages
+
+# Get the absolute path to the library
+lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src', 'python_wrapper', 'libmdlclient.so'))
+
+setup(
+    name="mdlin",
+    version="0.1.0",
+    packages=find_packages(),
+    package_data={
+        'mdlin': [lib_path],
+    },
+    include_package_data=True,
+    install_requires=[],
+    author="Austin Li",
+    description="MDLin Python Client Wrapper",
+    python_requires=">=3.6",
+)
